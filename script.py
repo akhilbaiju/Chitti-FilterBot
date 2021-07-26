be3e29de7258f5d6e67af70525cecddf381023ef
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-07-26 15:09:57 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,37 @@
+class Script(object):
+
+    START_MSG = """<b>ഹായ് {},
+എന്റെ പേര് Nagavalli! . Honey Bee Movies nte ആവിശ്യത്തിനായി എന്നെ  @akhilbaiju നിർമിച്ചത് ആണ്. എനിക്ക് ആവിശ്യം ഇല്ലാതെ മെസ്സേജ് അയക്കണ്ട എനിക്ക് ഗ്രൂപ്പിലെ മെസ്സേജ് മാത്രമേ കാണാൻ സാധിക്കുകയുള്ളു
+
+
+എന്നെ കുറിച്ച് കൂടുതൽ അറിയാൻ /help അമർത്താവുന്നതാണ്</b>
+"""
+
+
+    HELP_MSG = """
+<i>I can store unlimited movie 🎬 links. I'm completely made in Python 🐍 . I'm Using Mongo DB 🌱 as Database. Me or My boss do not own any content posted. We only share those files & links which are already shared by somebody else on the Internet 
+If you own the copyrights of any stuff intimate us and we will remove it ⚠️
+
+© ID to contact : @HBMCCBot</i>
+
+
+<b>My Basic Commands</b>
+
+/start - Check if I'm alive! 🥵
+/help - For More Information 🤓🤏🏻
+/about - Something about me! 🥰
+/id  -  I'll Show your  information 🤖
+/status  -  Shows current status (Only for my <a href='https://t.me/akhilbaiju/'>God</a>) 😉
+
+"""
+
+
+    ABOUT_MSG = """😎 <b>My Name : Nagavalli </b>
+
+🧑🏻‍💻 <b>Creater :</b> @AkhilBaiju
+
+🐍<b>Language :</b> <code>Python3</code>
+
+📕<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+
+"""
